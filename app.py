7d8a1d17c9c6f70bfd0861c7afbccc79d5acdf00
Giorgi Tsomaia <6746065+comcoma@users.noreply.github.com>
2023-04-17 14:53:22 +0400

    retrieves ideas from db. sign up and register added

--- app.py
+++ app.py
@@ -1,35 +1,27 @@
 import os
-
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
-
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+# Connect to DB
+db = SQL("sqlite:///cloud.db")
 
 
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
+
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
@@ -37,24 +29,11 @@ def after_request(response):
 
 
 @app.route("/")
-@login_required
 def index():
-    """Show portfolio of stocks"""
-    return apology("TODO")
+    """Show all the ideas posted"""
 
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock"""
-    return apology("TODO")
-
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions"""
-    return apology("TODO")
+    rows = db.execute("SELECT * FROM ideas")
+    return render_template("index.html", rows=rows)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -97,28 +76,48 @@ def login():
 def logout():
     """Log user out"""
 
-    # Forget any user_id
+    # Forget any user_id and redirect to home
     session.clear()
-
-    # Redirect user to login form
     return redirect("/")
-
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    return apology("TODO")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
 
+    session.clear()
+    # User reached route via POST (submitting the register form)
+    if request.method == "POST":
 
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Sell shares of stock"""
-    return apology("TODO")
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # ensure passwords match
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+
+        # save username and password hash in variables
+        username = request.form.get("username")
+        hash = generate_password_hash(request.form.get("password"))
+
+        # Query database to ensure username isn't already taken
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=username)
+        if len(rows) != 0:
+            return apology("username is already taken", 400)
+
+        # insert username and hash into database
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",
+                   username=username, hash=hash)
+
+        # redirect to login page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
